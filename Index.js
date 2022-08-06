ba28adce5bb607c2c983bfa643c24b7ce99fa625
Ryan9393 <mryan9393@gmail.com>
2022-08-06 19:12:07 -0400

    Works

--- Index.js
+++ Index.js
@@ -2,21 +2,15 @@
 // a.	A Bootstrap styled table representing your choice of data.
 // b.	A Bootstrap styled form that allows a user to add a new row to the table when submitted.
 
-const myForm = document.getElementById('myForm');
-const email =document.getElementById('email');
-const name = document.getElementById('name');
-
-const content = document.getElementById('content');
-
-const emailInfo = [
-    {email: 'joe@gmail.com', name: "Joe Smo"},
-    {email: 'John@gmail.com', name: "John Smith"}
-];
-
-emailInfo.forEach(person => {
-    const infoBox = document.createElement('div');
-    infoBox.cLassList.add('info-box');
-
-    infoBox.innerText = `${person.email} ${person.name}`;
-    content.append(info);
-})
+document.getElementById("submit").onclick = function () {
+    document.getElementById("table").style.display = "block";
+  
+    var table = document.getElementById("table");
+    var row = table.insertRow(-1);
+    var desc = row.insertCell(0);
+    var amt = row.insertCell(1);
+    desc.innerHTML = document.getElementById("first").value;
+    amt.innerHTML = document.getElementById("last").value;
+  
+    return false;
+  }
